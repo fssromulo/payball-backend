ff7b592c8875c5d2dbffb40cc2abbe7948e9324b
Rômulo Fernando <fssromulo@gmail.com>
2020-02-03 00:23:29 -0300

    aws rds database connection

--- src/database/database-connection.js
+++ src/database/database-connection.js
@@ -1,12 +1,25 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
    'payball',
-   'backup',
-   'UniSeguro',
+   'root',
+   'romuloroot',
    {
       dialect: 'mysql',
-      host: 'localhost'
+      host: 'payball2.clnkn1jxykut.us-east-2.rds.amazonaws.com'
    }
 );
 
 module.exports = sequelize;
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize(
+//    'payball',
+//    'backup',
+//    'UniSeguro',
+//    {
+//       dialect: 'mysql',
+//       host: 'localhost'
+//    }
+// );
+
+// module.exports = sequelize;
