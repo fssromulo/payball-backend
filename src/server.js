5248e9fee3a4f12af01ee0767e4bf9a9d208a51c
Rômulo Fernando <fssromulo@gmail.com>
2020-02-03 00:42:38 -0300

    env port backend

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const port = process.env.PORT || 3001;
+
 app.use('/', require('./routes'));
-app.listen(3001);
+app.listen(port);
+
 
 // http://localhost:3001/player
